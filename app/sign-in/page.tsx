67d1d5a6db7fedc95882509e926d46a5cb17a96e
charan-det-dev <charan.det.dev@gmail.com>
2025-04-29 22:53:43 +0700

    feat: Enhance sign-in page with styling and Cloudflare attribution
    This commit enhances the sign-in page with improved styling and adds an attribution to Cloudflare for server hosting.
    
    - Updates the `app/users/page.css` to change the flex direction to column.
    - Adds basic body styling in `app/sign-in/sign-in.css` and styles for the Cloudflare attribution.
    - Modifies the `app/sign-in/page.tsx` to include the Cloudflare logo and developer attribution.
    - Adds a line break for better UI.

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 import "./sign-in.css";
 
 export default function SignInPage() {
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
@@ -12,7 +11,6 @@ export default function SignInPage() {
     e.preventDefault();
 
     try {
-
       const response = await fetch("/api/sing-in", {
         method: "POST",
         headers: {
@@ -22,18 +20,14 @@ export default function SignInPage() {
       });
 
       if (response.ok) {
-        
         console.log("✅ Sign-in successful");
         window.location.href = "/"; // 🔄 redirect แล้ว reload หน้าใหม่
         // router.push("/");
-
       } else {
         const data = await response.json();
         setError(data.message || "❌ Invalid credentials");
       }
-
     } catch (error) {
-
       console.error("Sign-in error", error);
       setError("❌ Error occurred during sign-in.");
     }
@@ -41,6 +35,7 @@ export default function SignInPage() {
 
   return (
     <div className="sign-in-container">
+      <br />
       <h2>Sign In</h2>
       <form onSubmit={handleSubmit}>
         <div>
@@ -61,10 +56,28 @@ export default function SignInPage() {
             required
           />
         </div>
-        <button className="sign-in-button sign-in" type="submit">Sign In</button>
+        <button className="sign-in-button sign-in" type="submit">
+          Sign In
+        </button>
       </form>
-
+      <br />
       {error && <p style={{ color: "red" }}>{error}</p>}
+      <br />
+      <div className="sign-in-image">
+      <p className="sign-in-cloudflare">Server By</p>
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Cloudflare_Logo.svg/1024px-Cloudflare_Logo.svg.png?20220519022010"
+          alt="user"
+          width={100}
+          height={100}
+          className=""
+        />
+      </div>
+      <div className="sign-in-developer-name">
+        <p>Developer By D.Charan & API Mock By mockapi.io</p>
+        </div>
+      <br />
     </div>
   );
+
 }
